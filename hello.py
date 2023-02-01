bbf91a9ff1ed2d9f64b84b64a10bab77b6e89b4a
momerio <momerio601@gmail.com>
2023-02-01 22:56:30 +0900

    ボタンを押すと、時間が進むように変更 Fixes #2

--- hello.py
+++ hello.py
@@ -1,5 +1,28 @@
 import flet
 import time
+import math
+
+
+def _time_conversion(t: int):
+    """カンマ秒から時間文字列に変換
+
+    Args:
+        t (int): カンマ秒（最小時間）
+
+    Returns:
+        str: 時間文字列
+    """
+    splits_: str = str(math.floor(t % 1000)).zfill(3)
+    t: int = t // 1000
+    seconds_: str = str(math.floor(t % 60)).zfill(2)
+    t: int = t // 60
+    minutes_: str = str(math.floor(t % 60)).zfill(2)
+    t: int = t // 60
+    hours_: str = str(math.floor(t % 60)).zfill(1)
+
+    time_str: str = f"{hours_}:{minutes_}:{seconds_}.{splits_}"
+
+    return time_str
 
 
 def main(page: flet.Page):
@@ -9,7 +32,7 @@ def main(page: flet.Page):
         page (flet.Page): _description_
     """
     # windowタイトル
-    page.title = "Hello World"
+    page.title = "COUNT UP TIMER"
 
     # windowサイズ
     page.window_width = 600
@@ -23,23 +46,32 @@ def main(page: flet.Page):
     page.vertical_alignment = flet.MainAxisAlignment.CENTER
 
     # Textコントロール（一般的にはLabelと呼ばれるもの）
-    t = "Hello world!"
+    t = "COUNT UP TIMER"
     text = flet.Text(value=t, size=50, color="gray")
 
-    # クリックイベント
+    IS_START = True  # タイマー開始中フラグ
+
+    # 時間
+    hours = 0  # 時間
+    minutes = 0  # 分
+    seconds = 0  # 秒
+    splits = 0  # カンマ秒
+
+    # スタート・ストップボタン
     def start_button(e):
         time_ = 0
-        while True:
+        while IS_START:
             time_ += 1
-            print(time_)
-            time.sleep(1)
-            text.value = str(time_)
+            text.value = _time_conversion(time_)
+            time.sleep(0.001)
             page.update()
 
-    # クリックイベント
-    def stop_button(e):
+    # リセットボタン
+    def reset_button(e):
+        global IS_START
         time_ = 0
         text.value = str(time_)
+        IS_START = False
         page.update()
 
     # ページにコントロールを追加
@@ -52,8 +84,12 @@ def main(page: flet.Page):
         flet.Row(
             [
                 # アイコンボタン
-                flet.IconButton(flet.icons.PLAY_CIRCLE_FILL_OUTLINED, icon_size=50, on_click=start_button),
-                # flet.IconButton(flet.icons.PLAY_CIRCLE_FILL_OUTLINED, icon_size=50, on_click=stop_button),
+                flet.IconButton(
+                    icon=flet.icons.RESTART_ALT_OUTLINED, icon_size=50, tooltip="reset", on_click=reset_button
+                ),  # リセットボタン
+                flet.IconButton(
+                    flet.icons.PLAY_CIRCLE_FILL_OUTLINED, icon_size=50, tooltip="start", on_click=start_button
+                ),  # スタートボタン
             ],
             # 横位置をセンターにする
             alignment=flet.MainAxisAlignment.CENTER,
